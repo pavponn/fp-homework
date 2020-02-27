4f14390e7057f9cabffedc2da6a99b3db6b15261
Pavel Ponomarev <pavponn@yandex.ru>
2020-02-27 23:22:26 +0300

    Change finonacci and factorial

--- hw0/src/Task4.hs
+++ hw0/src/Task4.hs
@@ -12,15 +12,25 @@ iterateElement :: a -> [a]
 iterateElement = fix (\f -> \x -> x : (f x))
 
 -- |This function returns n'th number of Fibonacci.
--- In case argument is less then 1 returns 1.
 fibonacci :: Integer -> Integer
 fibonacci = fix $ \f n ->
-  if (n <= 2) then 1 else (f (n - 1)) + (f (n - 2))
+  if (n < 0)
+    then ((-1) ^ (abs n + 1)) * (f (abs  n))
+  else if n == 0
+    then 0
+  else if n <= 2
+    then 1
+  else (f (n - 1)) + (f (n - 2))
 
 -- |This functions return factorial of a given number.
--- If provided arguments is less then 1 returns 1.
+-- If provided argument is less then 0 returns undefined.
 factorial :: Integer -> Integer
-factorial = fix $ \f n -> if n <= 0 then 1 else (f (n - 1)) * n
+factorial = fix $ \f n ->
+  if n < 0
+    then undefined
+  else if (n == 0)
+    then 1
+  else n * f (n - 1)
 
 -- |The 'mapFix' function applies function to the elements of list.
 mapFix :: (a -> b) -> [a] -> [b]
